4868eb04e8e7cf9a1729a50f27f069a3d4ba732f
Jorge Saa <piojo.sistematico@gmail.com>
2024-05-09 21:24:23 -0400

    finished backend

--- backend/server.ts
+++ backend/server.ts
@@ -12,14 +12,53 @@ const upload = multer({ storage: storage });
 
 app.use(cors());
 
+/* variable to store data */
+
+let jsonData: Array<Record<string, string>> = [];
+
 app.post("api/files", upload.single("file"), async (req, res) => {
   console.log("posting");
+  const { file } = req;
+  /* Handle upload failed */
+  if (!file) {
+    return res.status(500).json({ message: "Upload failed" });
+  }
+
+  /* Check mimetype */
+  if (file.mimetype != "text/csv") {
+    return res.status(500).json({ message: "File must be CSV" });
+  }
+
+  /* Transform data */
+  try {
+    const csvData = file.buffer.toString();
+    console.log(csvData);
+    jsonData = csvToJSON.csvStringToJson(csvData);
+    console.log(jsonData);
+  } catch (error) {
+    return res.status(500).json({ message: "Error parsing the file" });
+  }
+
+  /* All good */
   return res.status(200).json({ message: "File uploaded successfully" });
 });
 
 app.get("api/users", async (req, res) => {
   console.log("getting");
-  return res.status(200).json({ data: [] });
+  const { q } = req.query;
+  if (!q) {
+    return res.status(500).json({ message: "Query param is missing" });
+  }
+
+  const searchWord = q.toString().toLowerCase();
+
+  const filteredData = jsonData.filter((elem) => {
+    return Object.values(elem).some((value) =>
+      value.toLowerCase().includes(searchWord)
+    );
+  });
+
+  return res.status(200).json({ data: filteredData });
 });
 
 app.listen(PORT, () => {
