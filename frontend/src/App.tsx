d1db4fc5e5b9f4f1fa583df8e7b69e9680066c01
Jorge Saa <piojo.sistematico@gmail.com>
2024-05-10 21:34:19 -0400

    started working on the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <main>
+        <h1>Upload and Search CSV</h1>
+        <form action="">
+          <input type="file" accept=".csv" />
+          <button>Upload</button>
+        </form>
+      </main>
+    </>
+  );
+}
+
+export default App;
